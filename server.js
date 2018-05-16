8e563d75ae56010e5f1976d2de2c62498bf79a0e
Anderson Tsang <andyderson@gmail.com>
2018-05-16 11:10:23 -0700

    Implemented Server-Side Rendering

--- server.js
+++ server.js
@@ -1,26 +1,79 @@
-const express = require('express');
-// const morgan = require('morgan');
+// <<<<<<<<<<<<<<<<<<<<<<<<< Server Side Rendering >>>>>>>>>>>>>>>>>>>>>>>>
+const express = require('express')
+const morgan = require('morgan');
 const path = require('path');
-
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// app.use(morgan('dev'));
-const bodyParser = require('body-parser');
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+const clientBundles = './public/services';
+const serverBundles = './templates/services';
+const serviceConfig = require('./service-config.json');
+const services = require('./loader.js')(clientBundles, serverBundles, serviceConfig);
 
-app.use(bodyParser.json());
+const React = require('react');
+const ReactDom = require('react-dom/server');
+const Layout = require('./templates/layout');
+const App = require('./templates/app');
+const Scripts = require('./templates/scripts');
 
+const renderComponents = (components, props = {}) => {
+  return Object.keys(components).map(item => {
+    let component = React.createElement(components[item], props);
+    return ReactDom.renderToString(component);
+  });
+};
 
-app.use('/restaurants', express.static(path.join(__dirname, './public')));
-
-app.get('/restaurants/:id', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+app.get('/restaurants/:id', function(req, res) {
+  console.log('this is services', services);
+  let components = renderComponents(services, {itemid: req.params.id});
+  res.end(Layout(
+    'Zaget Reviews',
+    App(...components),
+    Scripts(Object.keys(services))
+  ));
 });
-
-app.use(express.static(path.join(__dirname, './public')));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
+
+//<<<<<<<<<<<<<<<< Original >>>>>>>>>>>>>>>>>>>>>>>>>>>
+// // const newrelic = require('newrelic');
+// const express = require('express');
+// const axios = require('axios');
+// // const morgan = require('morgan');
+// const path = require('path');
+// const app = express();
+// const port = 3000;
+// // app.use(morgan('dev'));
+// const bodyParser = require('body-parser');
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use(bodyParser.json());
+
+// app.use('/restaurants', express.static(path.join(__dirname, './public')));
+
+// // Original
+// app.get('/restaurants/:id', (req, res) => {
+//   res.sendFile(path.join(__dirname, './public/index.html'));
+// });
+
+// app.use(express.static(path.join(__dirname, './public')));
+
+// app.listen(port, () => {
+//   console.log(`server running at: http://localhost:${port}`);
+// });
+
+// <<<<<<<<<<<<<<<<<<<<<<<< SSR >>>>>>>>>>>>>>>>>>>>>>>>>
+// app.get('/restaurants/:id', (req, res) => {
+//   axios.get(`http://54.183.72.230:3003/api/restaurants/${req.params.id}`)
+//   .then((data) => {
+//     var obj = {
+    
+//     }
+//     res.send()
+// });
---
+++ templates/app.js
@@ -0,0 +1,10 @@
+module.exports = (app) => `
+<div id="App">${app}</div>`;
+
+
+{/* <div id="app"></div>
+<div class="proxy-container"><div>
+<div id="description"></div>
+<div id="reviews"></div></div>
+<div id="apateezSidebar"></div></div>
+<div id="nearby-app"></div> */}
